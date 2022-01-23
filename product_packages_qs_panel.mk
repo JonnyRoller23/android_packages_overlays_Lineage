b4e634a0115e6a4bd581ec0f6862e39716000e22
JonnyRoller23 <maxim55miui11@gmail.com>
2022-01-24 01:14:42 +0300

    overlays: Added OS Panel overlay

---
+++ product_packages_qs_panel.mk
@@ -0,0 +1,8 @@
+PRODUCT_PACKAGES += \
+    QSPanel32Overlay \
+    QSPanel33Overlay \
+    QSPanel34Overlay \
+    QSPanel42Overlay \
+    QSPanel43Overlay \
+    QSPanel53Overlay \
+    QSPanel55Overlay \
