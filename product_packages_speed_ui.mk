5a07af3d9732b0093a004d33696c80df37d950c7
JonnyRoller23 <maxim55miui11@gmail.com>
2022-01-24 02:19:07 +0300

    overlays: Added SpeedUI overlays

---
+++ product_packages_speed_ui.mk
@@ -0,0 +1,8 @@
+PRODUCT_PACKAGES += \
+    Level1SpeedUIOverlay \
+    Level2SpeedUIOverlay \
+    Level3SpeedUIOverlay \
+    Level4SpeedUIOverlay \
+    Level5SpeedUIOverlay \
+    Level6SpeedUIOverlay \
+    Level7SpeedUIOverlay \
